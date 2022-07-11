a94ba88384dc42f1b7ee3d27fe7d03fc54653d44
DennisGambini <dennis.gambini@gmail.com>
2022-07-11 11:41:49 +0200

    order model e migration

--- app/Item.php
+++ app/Item.php
@@ -12,4 +12,8 @@ class Item extends Model
     public function category(){
         return $this->belongsTo('App\Category');
     }
+
+    public function orders(){
+        return $this->belongsToMany('App/Order');
+    }
 }
