eab5d6a4a0591ab46a642400e49b142cb8063334
Giuseppe <giuseppecantafio@hotmail.it>
2022-07-15 18:14:24 +0200

    prova validazione con js

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -5,3 +5,48 @@
  */
 
 require('./bootstrap');
+
+const checkAddress = document.querySelector('.check-address');
+
+checkAddress.addEventListener("click", function (event) {
+
+    event.preventDefault();
+
+    const address = document.getElementById('address');
+    const formAddress = document.getElementsByTagName('form');
+
+
+    // validazioni di address
+    let numbers = [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    ];
+    console.log(numbers);
+
+    let vie = [
+        'Via', 'Piazza', 'Largo', 'Strada', 'Stradone', 'Contrada', 'Rione', 'Circonvallazione', "Ca'"
+    ];
+    console.log(vie);
+
+    let numberChecked = false;
+    let viaChecked = false;
+
+    numbers.forEach((number) => {
+        if (address.value.includes(number)) {
+            numberChecked = true;
+        }
+    });
+
+    vie.forEach((via) => {
+        if (address.value.includes(via)) {
+            viaChecked = true;
+        }
+    });
+
+    if (viaChecked && numberChecked) {
+
+        formAddress[0].submit();
+
+    } else {
+        console.log("LEONARODOSODS")
+    };
+});
--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -5,13 +5,13 @@
 <div class="container">
   <h1>CREA RISTORANTE</h1>
   
-  <form action="{{route('admin.restaurants.store')}}" method="post" enctype="multipart/form-data">
+  <form action="{{route('admin.restaurants.store')}}" method="post" enctype="multipart/form-data" class="js-form-check">
       @csrf
   
     <div class="form-group mb-3">
       <label for="name">Nome</label>
-      <span class="mx-2">*</span>
-      <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="Inserisci nome" name="name" value="{{old('name')}}" required>
+      <span class="mx-2">* <small class="mx-2" style="color: grey">Max 100 caratteri</small></span>
+      <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="Inserisci nome" name="name" value="{{old('name')}}" maxlength="100" title="Il ristorante può contenere massimo 100 caratteri" required>
       @error('name')
           <div class="alert alert-danger">{{ $message }}</div>
       @enderror
@@ -68,7 +68,7 @@
       </div>
   
       <div class="d-flex justify-content-between">
-        <button type="submit" class="btn btn-primary">Invia</button>
+        <button type="submit" class="btn btn-primary check-address">Invia</button>
 
         <div>* Campi Obbligatori</div>
       </div>
@@ -76,4 +76,5 @@
 
 </div>
 
+<script src="{{asset('js/admin.js')}}"></script>
 @endsection
