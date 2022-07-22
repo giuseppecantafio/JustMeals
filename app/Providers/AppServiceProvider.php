724a452d8cac85ddcbea83e55685c7ea652b0971
DennisGambini <dennis.gambini@gmail.com>
2022-07-22 17:12:21 +0200

    env-braintree

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,9 +25,9 @@ class AppServiceProvider extends ServiceProvider
     {
         return $gateway = new \Braintree\Gateway([
             'environment' => 'sandbox',
-            'merchantId' => '794b5xxkqfnjy5hw',
-            'publicKey' => 'pymzsr68k8zspdgg',
-            'privateKey' => '32dfe078fccdb975bee6703b118bb48d'
+            'merchantId' => getenv('BT_MERCHANT_ID'),
+            'publicKey' => getenv('BT_PUBLIC_KEY'),
+            'privateKey' => getenv('BT_PRIVATE_KEY')
         ]);
     }
 }
