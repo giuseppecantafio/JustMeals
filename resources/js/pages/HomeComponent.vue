12d31af9d56cf82e5577d8a6abdc7f6e6cb4bf34
naiadidub <antonio.zaghi1405@gmail.com>
2022-07-25 09:51:23 +0200

    carosell home

--- resources/js/pages/HomeComponent.vue
+++ resources/js/pages/HomeComponent.vue
@@ -1,17 +1,75 @@
 <template>
     <div>
-        <h1 class="text-center">Deliveboo</h1>
-        <section class="my-5"></section>
-        <section class="my-5"></section>
-        <section></section>
+        <div class="container">
+            <VueSlickCarousel :arrows="true" :dots="true" v-bind="settings" v-if="ok">
+                <div v-for="n in pizzerie" :key="n.id">{{n.name}}</div>
+            </VueSlickCarousel>
+        </div>
     </div>
 </template>
 
 <script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+  // optional style for arrows & dots
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+
 export default {
     components: {},
     name: "HomeComponent",
+    components: { VueSlickCarousel },
+    data(){
+        return{
+            settings:{
+                "dots": true,
+                "focusOnSelect": true,
+                "infinite": true,
+                "speed": 500,
+                "slidesToShow": 3,
+                "slidesToScroll": 3,
+                "touchThreshold": 5,
+                "autoplay": true,
+                "speed": 8000,
+                "autoplaySpeed": 2000,
+                "cssEase": "linear"
+            },
+            pizzerie: [],
+            giapponesi: [],
+            ok: false,
+        }
+        
+    },
+    methods:{
+        getPizzerie(){
+            axios.get(`api/restaurants?typology=Pizzeria`)
+                    .then((response)=>{
+                        this.pizzerie = response.data;
+                        if (this.pizzerie.length > 0) {
+                            this.ok = true;
+                        }
+                    }).catch((error)=>{
+                        console.log(error);
+                    });
+        },
+        getGiapponesi(){
+            axios.get(`api/restaurants?typology=Giapponese`)
+                    .then((response)=>{
+                        this.giapponesi = response.data;
+                    }).catch((error)=>{
+                        console.log(error);
+                    });
+        },
+        
+        
+    },
+    
+    created(){
+        this.getPizzerie();
+        this.getGiapponesi();
+    },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+</style>
